c98053b4d64d7bb1f4edc13d0603269f1852df74
Krsiak Daniel <krsiak.daniel@gmail.com>
2020-04-08 19:56:07 +0200

    feature - search (#41)
    * content cleanup
    
    * search bar
    
    * search function + using axios
    
    * fixed props
    
    * results - display fetched movies

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,105 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import {
-  Box,
-  Heading,
-  Image,
-  Input,
-  SimpleGrid,
-  Skeleton,
-  Text,
-} from '@chakra-ui/core';
+import { Box, Heading } from '@chakra-ui/core';
+import axios from 'axios';
+
+import Search from '../components/Search';
+import Results from '../components/Results';
 
 const Movies = () => {
+  const [state, setState] = useState({
+    searchQuery: '',
+    results: [],
+  });
+  const apiUrl = 'http://www.omdbapi.com/?apikey=6fc8b664';
+  // example: http://www.omdbapi.com/?apikey=6fc8b664&s=batman
+
+  const search = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      axios(apiUrl + '&s=' + state.searchQuery).then(({ data }) => {
+        let results = data.Search;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+
+        console.log(data);
+      });
+    }
+  };
+
+  const handleInput = (e: any) => {
+    let searchValue = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, searchQuery: searchValue };
+    });
+  };
+
   return (
     <>
       <Box>
-        <Heading as="h1">Movies</Heading>
-        <Text>movies</Text>
+        <Heading as="h1" mb={4}>
+          Movies
+        </Heading>
       </Box>
-      <Box>
-        <Input placeholder="Search ..." size="lg" />
-      </Box>
-      <SimpleGrid
-        columns={{ sm: 1, md: 3, lg: 6 }}
-        spacingX="16px"
-        spacingY="16px"
-      >
-        <Box>
-          <Image
-            src="https://image.tmdb.org/t/p/w500/k7Bg8WaFeSMWwxCKX2BhGjNMTjA.jpg"
-            alt="i"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://image.tmdb.org/t/p/w500/or1gBugydmjToAEq7OZY0owwFk.jpg"
-            alt="i"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://image.tmdb.org/t/p/w300/5LrI4GiCSrChgkdskVZiwv643Kg.jpg"
-            alt="i"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://image.tmdb.org/t/p/w500/pTpxQB1N0waaSc3OSn0e9oc8kx9.jpg"
-            alt="i"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://image.tmdb.org/t/p/w500/ooR8S0O0ZgxK2OBnT1GWHrr9w92.jpg"
-            alt="i"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="https://image.tmdb.org/t/p/w300/bYR8O1H1ZlME7Dm9ysfTYZnRDpw.jpg"
-            alt="i"
-          />
-        </Box>
-
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-        <Box>
-          <Skeleton height="80px" mb="16px" />
-        </Box>
-      </SimpleGrid>
+      <Search handleInput={handleInput} search={search} />
+      <Results results={state.results} />
     </>
   );
 };
